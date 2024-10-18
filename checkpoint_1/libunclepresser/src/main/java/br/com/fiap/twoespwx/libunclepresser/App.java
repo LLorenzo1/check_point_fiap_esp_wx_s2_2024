4cfb2e52b43dc8b6334e15c78c080a9cb22edf5c
LLorenzo1 <134668961+LLorenzo1@users.noreply.github.com>
2024-10-17 22:42:15 -0300

    Update App.java

--- checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
+++ checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
@@ -1,23 +1,37 @@
 /* 
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * Grupo: Batata Doce
+ * Grupo: LORENZO 
  * Autores: 
- *      - João da Silva     -   jaodasilva@example.com
- *      - Jose da Silva     -   zedasilva@example.com
- *      - Maria da Silva    -   marydasilva@example.com
- *      - Monica da Silva   -   nicadasilva@example.com
+ *      - Lorenzo Ferreira    -   RM97318
  */
 
 package br.com.fiap.twoespwx.libunclepresser;
 
+import java.io.IOException;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("LIB UNCLE PRESSER - GRUPO BATATA-DOCE");
+public class App {
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Uso correto: java -jar <caminho/para/arquivo.jar> <caminho/para/entrada.txt> <caminho/para/saida.txt>");
+            return;
+        }
+
+        String caminhoEntrada = args[0];
+        String caminhoSaida = args[1];
+
+        try {
+            String dadosEntrada = LeitorArquivo.lerArquivo(caminhoEntrada);
+            String dadosComprimidos = CompressorRLE.comprimir(dadosEntrada);
+            LeitorArquivo.escreverArquivo(caminhoSaida, dadosComprimidos);
+
+            double[] frequencias = FrequenciaNucleotideos.calcularFrequencias(dadosEntrada);
+            int tamanhoEntrada = dadosEntrada.length();
+            int tamanhoSaida = dadosComprimidos.length();
+
+            InterfaceTextual.exibirInterface(caminhoEntrada, caminhoSaida, tamanhoEntrada, tamanhoSaida, frequencias);
+
+        } catch (IOException e) {
+            System.out.println("Erro ao ler ou escrever arquivos: " + e.getMessage());
+        }
     }
 }
